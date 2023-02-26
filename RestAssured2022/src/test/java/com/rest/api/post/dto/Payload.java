99e581d9b59c257072c998daf1e290b5c1f09189
Mdsalman07 <mdsalman0726@gmail.com>
2023-02-26 10:20:38 +0530

    Comitting Code Refactoring Changes

---
+++ RestAssured2022/src/test/java/com/rest/api/post/dto/Payload.java
@@ -0,0 +1,43 @@
+package com.rest.api.post.dto;
+
+public class Payload {
+	
+	String summary;
+	String description;
+	IssueType issuetype;
+	Project project;
+	
+	public Payload(String summary,String description,IssueType issue,Project project) {
+		this.summary=summary;
+		this.description=description;
+		this.issuetype=issue;
+		this.project=project;
+	}
+	public String getSummary() {
+		return summary;
+	}
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public IssueType getIssuetype() {
+		return issuetype;
+	}
+	public void setIssuetype(IssueType issuetype) {
+		this.issuetype = issuetype;
+	}
+	public Project getProject() {
+		return project;
+	}
+	public void setProject(Project project) {
+		this.project = project;
+	}
+	
+	
+
+}
---
+++ RestAssured2022/src/test/java/com/rest/api/post/dto/Project.java
@@ -0,0 +1,20 @@
+package com.rest.api.post.dto;
+
+public class Project {
+	
+	String key;
+	
+	public Project(String key) {
+		this.key=key;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+	
+
+}
